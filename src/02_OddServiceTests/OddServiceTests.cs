53d34dad725aeb9a3b70a92c1aeab484c054a9e2
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-04-25 10:30:45 +0100

    Implemented Odd Number Test Services

--- src/02_OddServiceTests/OddServiceTests.cs
+++ src/02_OddServiceTests/OddServiceTests.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using _02_OddService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace _02_OddServiceTests
 {
     public class OddServiceTests
     {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
 
+        public void IsOddMethod_OddNumbers_ReturnsTrue(int number)
+        {
+            var service = new OddServiceImplementation();
+
+            var result = service.IsOdd(number);
+
+            Assert.True(result);
+        }
+
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void IsOddMethod_EvenNumbers_ReturnsFalse(int number)
+        {
+            var service = new OddServiceImplementation();
+
+            var result = service.IsOdd(number);
+
+            Assert.False(result);
+        }
     }
 }
